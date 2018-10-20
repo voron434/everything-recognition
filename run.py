0220e1b11084c79d41d9ad18c32f227189153b4b
voron434 <070809010@mail.ru>
2018-10-20 10:47:49 +0300

    asdf

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import cv2
 
+from config import CASCADES
+
 
 def is_user_wants_quit():
     return cv2.waitKey(1) & 0xFF == ord('q')
@@ -14,17 +16,10 @@ def draw_sqare(frame, color):
 
 
 def get_cascades():
-    cascade_paths = {
-        "Face front": 'haarcascades/haarcascade_frontalface_default.xml',
-        "Face profile": 'haarcascades/haarcascade_profileface.xml',
-        # "Smile": 'haarcascades/haarcascade_smile.xml',
-        "Eyes": 'haarcascades/haarcascade_eye.xml',
-        # "Full body": 'haarcascades/haarcascade_fullbody.xml',
-        # "Cat face": 'haarcascades/haarcascade_frontalcatface_extended.xml',
-    }
     cascades = [
-        cv2.CascadeClassifier(cascade_path)
-        for title, cascade_path in cascade_paths.items()
+        (cv2.CascadeClassifier(cascade['path']), cascade['color'])
+        for title, cascade in CASCADES.items()
+        if cascade['draw']
     ]
     return cascades
 
@@ -37,15 +32,16 @@ if __name__ == "__main__":
             print("Couldn't find your webcam... Sorry :c")
         _, webcam_frame = video_capture.read()
         gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
-        captures = [cascade.detectMultiScale(
-            gray_frame,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
-        ) for cascade in cascades]
-        for capture in captures:
-            for (x, y, w, h) in capture:
-                draw_sqare(webcam_frame, (255, 0, 0))
+        for cascade, color in cascades:
+            captures = [cascade.detectMultiScale(
+                gray_frame,
+                scaleFactor=1.1,
+                minNeighbors=10,
+                minSize=(30, 30)
+            )]
+            for capture in captures:
+                for (x, y, w, h) in capture:
+                    draw_sqare(webcam_frame, color)
         show_frame(webcam_frame)
 
         if is_user_wants_quit():
